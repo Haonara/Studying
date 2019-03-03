be0af5e919a9df7bdb6c7d13ca2510d860a2aebc
haonara <masha-2007.555@mail.ru>
2019-03-03 14:49:59 +1000

    Сделана вторая задача

--- Java1Level.DZ2/src/ru/java1/dz2/Main.java
+++ Java1Level.DZ2/src/ru/java1/dz2/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
     //invertArray();
-
+    //fillArray();
 
     }
 
@@ -33,5 +33,11 @@ public class Main {
 
     }
 
-    
+    public static void fillArray (){
+        int [] arr=new int[8];
+        for (int i=0; i<8; i++) {
+            arr [i]=i*3;
+            System.out.println("arr["+i+"]="+arr[i]);
+        }
+    }
 }
