56a1ab0ef88b48b5cfe21caf5cb04f5732c576a1
haonara <masha-2007.555@mail.ru>
2019-03-03 14:27:15 +1000

    Начато выполнение дз

--- Java1Level.DZ2/src/ru/java1/dz2/Main.java
+++ Java1Level.DZ2/src/ru/java1/dz2/Main.java
@@ -1,8 +1,21 @@
 package ru.java1.dz2;
+import java.util.Scanner;
+import java.lang.Math;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+
+    }
+
+    public static void invertArray () {
+
+        System.out.println("Введите размерность массива:");
+        
+
     }
 }
