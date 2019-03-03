3890b48090b166f03d52b934d557ab57c39f370c
haonara <masha-2007.555@mail.ru>
2019-03-03 14:55:41 +1000

    Сделана третья задача

--- Java1Level.DZ2/src/ru/java1/dz2/Main.java
+++ Java1Level.DZ2/src/ru/java1/dz2/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     //invertArray();
     //fillArray();
+    //changeArray();
 
     }
 
@@ -40,4 +41,16 @@ public class Main {
             System.out.println("arr["+i+"]="+arr[i]);
         }
     }
+
+    public static void changeArray(){
+        int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<6) {
+                arr[i]=arr[i]*2;
+            }
+            System.out.println("arr["+i+"]="+arr[i]);
+        }
+
+    }
+
 }
