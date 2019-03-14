634e5b326a2b16dcaec4c92787d0048bb0bf3f16
Haonara <masha-2007.555@mail.ru>
2019-03-14 14:27:02 +1000

    Сделано

--- Homework5/src/ru/java1/dz5/People.java
+++ Homework5/src/ru/java1/dz5/People.java
@@ -8,7 +8,6 @@ public class People {
     String phone;
     int salary;
     int age;
-    People[] peoplesArray = new People[5];
 
     public People(String FIO, String dolzhnost, String email,String phone, int salary, int age){
         this.FIO=FIO;
@@ -19,15 +18,7 @@ public class People {
         this.age=age;
     }
 
-    public void info (){
-        System.out.println("Сотрудник "+FIO+ " "+age+ " лет, тел.: "+phone+ ", почта: "+email);
-        System.out.println("Занимает должность '"+dolzhnost+ "' с зарплатой в "+salary+" рублей.");
-    }
 
-    public static void show(People[] peoplesArray){
-        for (int i = 0; i < peoplesArray.length; i++) {
-            System.out.println(peoplesArray[i]);
-        }
-    }
+
 
 }
