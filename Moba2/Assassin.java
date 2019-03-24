ae430f772e978c6b259df971a0b87ebbb1727bd1
haonara <masha-2007.555@mail.ru>
2019-03-24 15:24:15 +1000

    Заново начат проект

---
+++ Moba2/Assassin.java
@@ -0,0 +1,7 @@
+package Lesson_7.Moba2;
+
+public class Assassin extends Hero{
+    public Assassin() {
+        super.Hero(80, 110, 0);
+    }
+}
---
+++ Moba2/Warrior.java
@@ -0,0 +1,8 @@
+package Lesson_7.Moba2;
+
+
+public class Warrior extends Hero{
+    public Warrior() {
+        super.Hero(50, 150, 0);
+    }
+}
