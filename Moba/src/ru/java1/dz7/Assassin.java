dace662c2233967dbe0a9fd4dd075b3ac6024140
haonara <masha-2007.555@mail.ru>
2019-03-24 15:36:48 +1000

    Удалено

---
+++ Moba/src/ru/java1/dz7/Assassin.java
@@ -0,0 +1,7 @@
+package ru.java1.dz7;
+
+public class Assassin extends Hero{
+    public Assassin() {
+        super.Hero(80, 110, 0);
+    }
+}
---
+++ Moba/src/ru/java1/dz7/Warrior.java
@@ -0,0 +1,7 @@
+package ru.java1.dz7;
+
+public class Warrior extends Hero{
+    public Warrior() {
+        super.Hero(50, 150, 0);
+    }
+}
