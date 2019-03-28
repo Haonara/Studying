fb390f6f867ca72dd57dc5f537c5f2a14be64131
Haonara <masha-2007.555@mail.ru>
2019-03-28 11:12:10 +1000

    Сделан запуск из окна настроек по кнопке

--- Moba/src/ru/java1/dz7/BattleGame.java
+++ Moba/src/ru/java1/dz7/BattleGame.java
@@ -4,37 +4,7 @@ import java.util.Arrays;
 
 public class BattleGame {
     static int activePlayerID = 0;
-
-    public static void main(String[] args) {
-        boolean finishGame = false;
-
-
-
-        Player[] players = {
-                new Player("Green", true, HeroesGenerator.generateRandomHeroes(Player.DEFAULT_HEROES_COUNT)),
-                new Player("Red", false, HeroesGenerator.generateRandomHeroes(Player.DEFAULT_HEROES_COUNT))
-        };
-
-        int idActive;
-        int idPassive;
-
-        while (!finishGame) {
-            idActive = BattleGame.activePlayerID;
-            idPassive  = ChangeActivePlayerID(idActive);
-            System.out.println("Атаковал игрок: " + players[idActive].getName() + ": " + Arrays.toString(players[idActive].getHeroes()));
-            System.out.println("Игрок " + players[idPassive].getName() + ": " + Arrays.toString(players[idPassive].getHeroes()));
-            if (players[idActive].isWinAfterAttack(players[idPassive])) {
-                System.out.println("Победил игрок: " + players[idActive].getName());
-                finishGame = true;
-                break;
-            }
-
-        }
-    }
-
-    public int getActivePlayerID() {
-        return activePlayerID;
-    }
+    public static String winner;
 
     public static int ChangeActivePlayerID(int activePlayerID) {
         switch (activePlayerID) {
@@ -50,4 +20,33 @@ public class BattleGame {
         }
         return BattleGame .activePlayerID;
     }
+
+    public static void start(){
+        boolean finishGame = false;
+
+        Player[] players = {
+                new Player("Team1", true, HeroesGenerator.generateRandomHeroes(Player.DEFAULT_HEROES_COUNT)),
+                new Player("Team2", false, HeroesGenerator.generateRandomHeroes(Player.DEFAULT_HEROES_COUNT))
+        };
+
+        int idActive;
+        int idPassive;
+
+        while (!finishGame) {
+            idActive = BattleGame.activePlayerID;
+            idPassive  = ChangeActivePlayerID(idActive);
+            System.out.println("Атаковал игрок: " + players[idActive].getName() + ": " + Arrays.toString(players[idActive].getHeroes()));
+            System.out.println("Игрок " + players[idPassive].getName() + ": " + Arrays.toString(players[idPassive].getHeroes()));
+            if (players[idActive].isWinAfterAttack(players[idPassive])) {
+                System.out.println("Победитель: " + players[idActive].getName());
+                winner=players[idActive].getName();
+                finishGame = true;
+                break;
+            }
+
+        }
+    }
+
 }
+
+
--- Moba/src/ru/java1/dz7/SettingsWindow.java
+++ Moba/src/ru/java1/dz7/SettingsWindow.java
@@ -2,12 +2,16 @@ package ru.java1.dz7;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
 
 public class SettingsWindow extends JFrame {
 
     public SettingsWindow(){
         setTitle("Settings");
-        setBounds(300, 300, 750, 450);
+        setBounds(300, 300, 400, 250);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
@@ -18,6 +22,9 @@ public class SettingsWindow extends JFrame {
         panelTeam1.add(comboBox1);
         JButton button1=new JButton("Добавить");
         panelTeam1.add(button1);
+        comboBox1.addItem("Warrior");
+        comboBox1.addItem("Assassin");
+        comboBox1.addItem("Healer");
 
         JPanel panelTeam2 = new JPanel();
         panelTeam2.setLayout(new BoxLayout(panelTeam2, BoxLayout.X_AXIS));
@@ -25,35 +32,68 @@ public class SettingsWindow extends JFrame {
         panelTeam2.add(comboBox2);
         JButton button2=new JButton("Добавить");
         panelTeam2.add(button2);
+        comboBox2.addItem("Warrior");
+        comboBox2.addItem("Assassin");
+        comboBox2.addItem("Healer");
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3,3));
         JLabel label1=new JLabel("Team1");
         JLabel label2=new JLabel("Team2");
+        label1.setMaximumSize(new Dimension(100,10));
+        label2.setMaximumSize(new Dimension(100,10));
         panel.add(label1);
         panel.add(label2);
         panel.add(panelTeam1);
         panel.add(panelTeam2);
-        JList listTeam1=new JList();
-        panel.add(listTeam1);
-        JList listTeam2=new JList();
-        panel.add(listTeam2);
+
+        JLabel list1=new JLabel();
+        JLabel list2=new JLabel();
+        list1.setMaximumSize(new Dimension(100,50));
+        list2.setMaximumSize(new Dimension(100,50));
+        panel.add(list1);
+        panel.add(list2);
         add(panel);
+        JPanel ending=new JPanel();
+        ending.setLayout(new GridLayout(1,2));
+        JLabel winner=new JLabel("Победитель: ");
         JButton start=new JButton("Начать");
         JTextField end=new JTextField(1);
+        start.setAlignmentX(Component.CENTER_ALIGNMENT);
+        end.setMaximumSize(new Dimension(50,10));
+        end.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(start);
-        add(end);
-
-
-
-
-
-
-
-
-
+        ending.add(winner);
+        ending.add(end);
+        ending.setMaximumSize(new Dimension(200,10));
+        add(ending);
 
         setVisible(true);
+
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               BattleGame.start();
+               end.setText(BattleGame.winner);
+            }
+        });
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               comboBox1.getSelectedItem();
+
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                comboBox2.getSelectedItem();
+            }
+        });
+
     }
 
 }
+
